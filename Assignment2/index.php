d8ce7107ff4c8460f460bb4970ecde0f00e9c9a2
Aarsh Patel <114701316+Aarsh0811@users.noreply.github.com>
2022-11-04 12:56:54 -0400

    Create index.php

---
+++ Assignment2/index.php
@@ -0,0 +1,99 @@
+<!--
+NAME : Aarsh Parimal Patel
+Assignment#2 
+Semester-2
+-->
+
+
+<!-- This is the code for the Home page of our website -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<!-- adding the metadata -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<title>Assignment 2</title>
+<meta name="description" content="Assignment 2">
+<meta name="robots" content="noindex,nofollow">
+
+<!-- providing the link for the fonts selected from google fonts -->
+
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet">
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&display=swap" rel="stylesheet">
+
+<!-- adding our css style sheet -->
+<link rel="stylesheet" href="./css/style.css">
+
+</head>
+
+<!-- adding our body -->
+<body>
+  <header>
+
+
+  <!-- adding logo for the header -->
+  <div class="headerlogo">
+    <a href="index.php">
+    <img src="./img/headerlogo.jpg" alt="logo"></a>
+  </div>
+
+<!-- adding a heading to our website -->
+  <div class="heading">
+    <h1>NGO</h1>
+  </div>
+
+
+  <!-- adding nav to give links to our other webpages -->
+  <nav>
+    <ul>
+     <li>
+       <a href="index.php">Home</a></li>
+     <li>
+      <a href="donate.php">Donate</a></li>
+     <li>
+      <a href="viewrecords.php">View Records</a></li>
+
+   </ul>
+  </nav>
+    </header>
+  <main>
+<!-- creating section to add information and images -->
+    <section>
+      <div class="img1">
+        <img src="./img/img1.jpg" alt="img1">
+
+      </div>
+      <div class="about">
+        <h2>ABOUT THE NGO</h2>
+
+      </div>
+      <div class="info1">
+        <img src="./img/img2.jpg" alt="img2">
+        <p> A non-governmental organization (NGO) or non-governmental organisation (see spelling differences) is an organization that generally is formed independent from government. They are typically nonprofit entities, and many of them are active in humanitarianism or the social sciences; they can also include clubs and associations that provide services to their members and others. Surveys indicate that NGOs have a high degree of public trust, which can make them a useful proxy for the concerns of society and stakeholders.However, NGOs can also be lobby groups for corporations, such as the World Economic Forum</p>
+      </div>
+      <h3>“It’s easier to take than to give. It’s nobler to give than to take. The thrill of taking lasts a day. The thrill of giving lasts a lifetime. -Joan Marques</h3>
+<div class="info2">
+  <p>NGOs act as implementers, catalysts, and partners. They mobilize resources to provide goods and services to people who have been affected by a natural disaster; they drive change, and partner with other organizations to tackle problems and address human needs.NGOs vary by method; some are primarily advocacy groups, and others conduct programs and activities. Oxfam, concerned with poverty alleviation, may provide needy people with the equipment and skills to obtain food and drinking water; the Forum for Fact-finding Documentation and Advocacy (FFDA) helps provide legal assistance to victims of human-rights abuses. The Afghanistan Information Management Services provide specialized technical products and services to support development activities implemented on the ground by other organizations. Management techniques are crucial to project success.</p>
+  <img src="./img/img3.jpg" alt="img3">
+</div>
+    </section>
+  </main>
+
+  <!-- adding our footer -->
+<footer>
+
+  <section>
+    <h2>END OF THE PAGE</h2>
+  </section>
+
+</footer>
+  </body>
+</html>
+
+<!-- end of code for home page -->
